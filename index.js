693d56ff80fdebf24c7b9f83c7ceaa52bde6368f
shannon7777 <shannonsimoncherry@gmail.com>
2023-01-12 23:22:29 +0800

    added function to scrap asus gpus on amazon

--- index.js
+++ index.js
@@ -1,15 +1,34 @@
 const express = require("express");
+const app = express();
 const path = require("path");
 const cors = require("cors");
 const dotenv = require("dotenv");
-const app = express();
 const PORT = process.env.PORT || 5050;
+const axios = require("axios");
+const { load } = require("cheerio");
 
 dotenv.config();
 app.use(cors());
 
-app.get("/", (req, res) => {
-  res.status(200).json({ message: "welcome to web scraper test" });
+app.get("/test/", async (req, res) => {
+  const amazonURL = `https://www.amazon.com/s?k=RX+6800&rh=n%3A284822&ref=nb_sb_noss`
+  const baseURL = `https://www.amazon.com`;
+  const gpuList = [];
+  
+  await axios
+    .get(amazonURL)
+    .then((response) => {
+      const html = response.data;
+      const $ = load(html);
+      $('a:contains(ASUS TUF)', html).each(function () {
+        const title = $(this).text();
+        const url = baseURL + $(this).attr("href");
+        // const price = $(this).
+        gpuList.push({ title, url });
+      });
+      res.json(gpuList);
+    })
+    .catch((err) => console.log(err));
 });
 
 app.listen(PORT, () => console.log(`This server is running on PORT:${PORT}`));
